7943bd42e9ddbae005a6cdca767e86ca05bccc7d
ChristianBianchi99 <bianchi848@gmail.com>
2023-07-27 17:50:27 +0200

    Agg nuovi dati all'interno del db - Es concluso

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -16,6 +16,20 @@ class TrainsTableSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        //
+        for($i=0; $i<10; $i++){
+            $newTrain = new Train();
+            $newTrain->Azienda = $faker->word();
+            $newTrain['Stazione di Partenza'] = $faker->word();
+            $newTrain['Stazione di Arrivo'] = $faker->word();
+            $newTrain['Data di Partenza'] = $faker->date();
+            $newTrain['Orario di Partenza'] = $faker->time();
+            $newTrain['Orario di Arrivo'] = $faker->time();
+            $newTrain['Codice Treno'] = $faker->randomNumber(4, true);
+            $newTrain['Numero Carrozze'] = $faker->randomDigit();
+            $newTrain['In Orario'] = $faker->boolean();
+            $newTrain['Cancellato'] = $faker->boolean();
+            
+            $newTrain->save();
+        };
     }
 }
